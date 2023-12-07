0a2205a34ae56cae252f3efe6858646d45963606
johan1103 <johan1103@naver.com>
2023-12-08 01:23:33 +0900

    [Feat] redis container

--- release-process.sh
+++ release-process.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 sudo docker compose pull web1 -q
-sudo docker compose up --force-recreate -d
+sudo docker compose up --force-recreate -d web1
 
 echo -e "\033[32m"api-server 배포 성공"\033[0m" 
